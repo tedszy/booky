6f4efeec5d4bdcf4f6e115099d227d1abea4aac3
Ted Szylowiec <tedszy@gmail.com>
2024-09-12 20:03:08 -0400

    PublicationTable class.

--- booky.py
+++ booky.py
@@ -1,28 +1,45 @@
 # booky.py
 #
-# Load publications.toml into data structure,
-# create in-memory sqlite3 database.
+# Load publications.toml into data structure.
+# Validate the data in the toml file.
+# Create in-memory sqlite3 database.
 
 import sqlite3
 import operator
-import tabulate
+from tabulate import tabulate
 
+# In case using python version < 3.11.
 try:
     import tomllib
 except ImportError:
     import toml as tomllib
 
 
-publication_filename = "publication.toml"
+class PublicationTable:
 
-with open(publication_filename, "rb") as f:
-    data = tomllib.load(f)
+    def __init__(self):
+        self.headers = ['key', 'title', 'BH', 'BW', 'CH', 'CW', 'CLR']
 
-headers = ['key', 'title', 'BH', 'BW', 'CH', 'CW', 'CLR']
-table = [[k, v['title'], v['block-height'], v['block-width'],
-             v['cover-height'], v['cover-width'], v['color']] for k,v in data.items()] 
+    def load(self, publication_filename):
+        self.filename = publication_filename
+        with open(self.filename, 'rb') as f:
+            self.data = tomllib.load(f)
+        self.table = [[k, v['title'], v['block-height'], v['block-width'],
+                          v['cover-height'], v['cover-width'], v['color']] 
+                      for k, v in self.data.items()] 
+        self.table_string = tabulate(sorted(self.table, 
+                                            key=operator.itemgetter(0), 
+                                            reverse=False), 
+                                     self.headers, 
+                                     tablefmt="grid", 
+                                     floatfmt=".4f")  
+             
 
-print(tabulate.tabulate(sorted(table, key=operator.itemgetter(0), reverse=False), 
-                        headers, tablefmt="grid", floatfmt=".4f"))  
+if __name__ == '__main__':
+    PT = PublicationTable()
+    PT.load("publication.toml")
+    print(PT.table_string)
+    print('Done.')
 
-print('done.')
+
+
