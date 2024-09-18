8e9f6771c46c57acaf10d33d6fe89be89afd530f
Ted Szylowiec <tedszy@gmail.com>
2024-09-18 10:15:32 -0400

    refactored, split pub db into 2 files, validation

--- booky.py
+++ booky.py
@@ -1,104 +1,149 @@
-# booky.py
-#
-# Load publications.toml into data structure.
-# Validate the data in the toml file.
-# Create in-memory sqlite3 database.
-#
-# * Multiple publication toml files.
-# * add prefix OBV to buckram colors.
 
+# booky2.py
 
-import argparse
-import operator
-import sqlite3
 import tomllib
 import logging
 import os.path
-from tabulate import tabulate
 
 
 logger = logging.getLogger('booky')
 logging.basicConfig(level=logging.DEBUG)
 
 
-class BadKey(Exception):
-    pass
+def check_color(color):
+    '''color is 3 character string.'''
+    return isinstance(color, str) and len(color)==3
 
 
-class MissingKey(Exception):
-    pass
+def check_limits(limits):
+    '''Sanity check for block and cover limits. '''
+    return (len(limits)==2 
+            and isinstance(limits[0], int) 
+            and isinstance(limits[1], int) 
+            and limits[0] < limits[1])
 
 
-class BadDBFilename(Exception):
-    pass
+def is_within_limits(x, limits):
+    '''limits [a,b], a <= x <= b'''
+    return (limits[0] <= x <= limits[1])
 
 
-class ConfigurationValidator:
-    '''Loads configuration settings into an instance.'''
-    
+def missing_keys(keys_A, keys_B):
+    '''Keys in keys_A that do not appear in keys_B.'''
+    return [x for x in keys_A if x not in keys_B]
+
+
+def extra_keys(keys_A, keys_B):
+    '''Keys in keys_B that are not in keys_A.'''
+    return [x for x in keys_B if x not in keys_A]
+
+
+class ValidationError(Exception):
+    '''Booky configuration or publication validation error.'''
+
+    def __init__(self, message, value, filename=None):
+        super().__init__()
+        self.message = message
+        self.value = value
+        self.filename = filename
+        if self.filename:
+            self.user_message = f"{message} '{value}' in file '{filename}'"
+        else:
+            self.user_message = f"{message} '{value}'"
+
+
+class ConfigError(ValidationError):
+    '''Bad or missing keys in config file.'''
+
+    def __str__(self):
+        return 'ConfigError... ' + self.user_message
+
+
+class PubError(ValidationError):
+    '''Bad publication data.'''
+
+    def __str__(self):
+        return 'PubError... ' + self.user_message
+
+
+class ConfigValidator:
+    '''Makes sure configure.toml is sane.'''
+
     config_filename = 'configure.toml'
-    toplevel_keys = ['database-filenames', 'validation']
+    toplevel_keys = ['pub-db-filenames', 'pub-fields', 'validation']
     validation_keys = ['block-limits', 'cover-limits', 'colors'] 
 
     def __init__(self):
 
-        # Handle possible missing or corrupt toml file with try... except.
-        with open('configure.toml', 'rb') as f:
-            data = tomllib.load(f)
+        try:
+            with open(self.config_filename, 'rb') as f:
+                data = tomllib.load(f)
+        except FileNotFoundError as fnf:
+            raise ConfigError('config file not found', 
+                              self.config_filename, None)
+        except tomllib.TOMLDecodeError as tde:
+            raise ConfigError('Unreadable TOML in config file', 
+                              self.config_filename, None)
 
-        logger.debug(f"{self.config_filename} data loaded.")
+        logger.debug(f"{self.config_filename} TOML read ok.")
 
-        # Check config data keys before proceeding.
-        
-        # Toplevel keys.
-        for k in data:
-            if k not in self.toplevel_keys:
-                raise BadKey(f"bad toplevel key: '{k}'.")
-        for k in self.toplevel_keys:
-            if k not in data:
-                raise MissingKey(f"missing toplevel key: '{k}'.")
+        # Check toplevel keys.
+        missing = missing_keys(self.toplevel_keys, data.keys())
+        extra = extra_keys(self.toplevel_keys, data.keys())
+        if len(missing) > 0:
+            raise ConfigError('Missing toplevel keys', 
+                              missing, self.config_filename)
+        if len(extra) > 0:
+            raise ConfigError('Extra toplevel keys', 
+                              extra, self.config_filename)
 
-        # Validation keys.
-        for k in data['validation']:
-            if k not in self.validation_keys:
-                raise BadKey(f"bad validation key: '{k}'.")
-        for k in self.validation_keys:
-            if k not in data['validation']:
-                raise MissingKey(f"missing validation key: '{k}'.")
+        logger.debug(f"{self.config_filename} toplevel keys ok.")
 
-        logging.debug(f"{self.config_filename} keys are OK.")
+        # Check validation keys.
+        missing = missing_keys(self.validation_keys, data['validation'].keys())
+        extra = extra_keys(self.validation_keys, data['validation'].keys())
+        if len(missing) > 0:
+            raise ConfigError('Missing validation keys', 
+                              missing, self.config_filename)
+        if len(extra) > 0:
+            raise ConfigError('Extra validation keys', 
+                              extra, self.config_filename)
 
-        # Check that publication database files exist.
-        for filename in data['database-filenames']:
+        logging.debug(f"{self.config_filename} validation keys ok.")
+
+        # Check that publication db files exist.
+        for filename in data['pub-db-filenames']:
             if not os.path.isfile(filename):
-                raise BadDBFilename(f"database file '{filename}' isn't in pwd.")
+                raise ConfigError("Pub db file doesn't exist:", 
+                                  filename, None)
 
-        # Check that colors are all 3-character numeric strings.
-        if all(isinstance(c, str) and len(c)==3 for c in data['validation']['colors']):
-            logging.debug(f"{self.config_filename} colors are OK.")
-        else:
-            raise ValueError(f"bad color value in {self.config_filename}.")
+        logging.debug(f"{self.config_filename} pub-db files exist ok.")
 
-        # Check that block-limits and cover-limits 
-        # are lists of two integers [a,b] with a < b.
-        u = data['validation']['block-limits']
-        if len(u)==2 and isinstance(u[0],int) and isinstance(u[1],int) and u[0]<u[1]:
-            logging.debug(f"{self.config_filename} block-limits are OK.")
-        else:
-            raise ValueError(f"bad block-limits in {self.config_filename}.")
-        u = data['validation']['cover-limits']
-        if len(u)==2 and isinstance(u[0],int) and isinstance(u[1],int) and u[0]<u[1]:
-            logging.debug(f"{self.config_filename} cover-limits are OK.")
-        else:
-            raise ValueError(f"bad cover-limits in {self.config_filename}.")
+        # Check colors.
+        for color in data['validation']['colors']:
+            if not check_color(color):
+                raise ConfigError(f"bad color value", color, self.config_filename)
 
-        self.pub_db_filenames = data['database-filenames']
+        logging.debug(f"{self.config_filename} colors are OK.")
+
+        # Check block-limits and cover-limits. 
+        block_limits = data['validation']['block-limits']
+        if not check_limits(block_limits):
+            raise ConfigError("Bad block limits", block_limits, self.config_filename)
+        cover_limits = data['validation']['cover-limits']
+        if not check_limits(cover_limits):
+            raise ConfigError("Bad cover limits", cover_limits, self.config_filename)
+
+        logging.debug(f"{self.config_filename} block and cover limits ok.")
+
+        self.pub_db_filenames = data['pub-db-filenames']
+        self.pub_fields = data['pub-fields']
         self.valid_colors = data['validation']['colors']
         self.block_limits = data['validation']['block-limits']
         self.cover_limits = data['validation']['cover-limits']
 
 
-class Publication:
+class Pub:
     '''Each instance holds the bookbinding specs of a publication db entry.'''
     
     def __init__(self, data):
@@ -109,16 +154,81 @@ class Publication:
         self.cover_width = data['cover-width']
         self.color = data['color']
 
+    def __repr__(self):
+        return f"<Pub {self.title}>"
 
-class PublicationValidator(ConfigurationValidator):
+
+class PubValidator(ConfigValidator):
     '''Validate contents of publication database files.
     Walk through each publication and check the sanity of the 
-    values in the data fields.'''
+    values in the data fields against what is specified
+    in the configuration file.'''
 
     unique_keys = []
 
     def __init__(self):
         super().__init__()
+       
+        # Maintain a list of publication keys for testing uniqueness.
+        self.pub_keys = []
+
+        # Maintain a dict of publication instances.
+        self.pubs = {}
+
+        # Go through publication data files and build self.pubs list. 
+        # TOML format requires unique keys, so within the file this
+        # is guaranteed.
+        for filename in self.pub_db_filenames:
+
+            # Check file existence and toml read.
+            try:
+                with open(filename, 'rb') as f:
+                    data = tomllib.load(f)
+            except FileNotFoundError:
+                raise PubError('Publication db file not found', filename, None)
+            except tomllib.TOMLDecodeError:
+                raise PubError('Unreadable TOML in Pub db file', filename, None)
+
+            for key, value in data.items():
+                # Check for duplicate key.
+                if key in self.pub_keys:
+                    raise PubError("Duplicate key", key, filename)
+                else:
+                    # Check that publication field keys are good.
+                    # Can make this more specific: show missing or extra field keys.
+                    if not set(self.pub_fields)==set(value.keys()):
+                        raise PubError("Bad or missing pub field key in", key, filename)
+                    
+                    # Check if pub color is valid.
+                    c = value['color']
+                    if c not in self.valid_colors:
+                        raise PubError("Bad color", (key, c), filename)
+                    
+                    # Check if block height is valid.
+                    bh = value['block-height']
+                    if not is_within_limits(bh, self.block_limits):
+                        raise PubError("Block height outside limits:", (key, bh), filename)
+                    
+                    # Check if block width is valid.
+                    bw = value['block-width']
+                    if not is_within_limits(bw, self.block_limits):
+                        raise PubError("Block width outside limits:", (key, bw), filename)
+                    
+                    # Check if cover height is valid.
+                    ch = value['cover-height']
+                    if not is_within_limits(ch, self.cover_limits):
+                        raise PubError("Cover height outside limits:", (key, ch), filename)
+
+                    # Check if cover width is valid.
+                    cw = value['cover-width']
+                    if not is_within_limits(cw, self.cover_limits):
+                        raise PubError("Cover height outside limits:", (key, cw), filename)
+
+                    # Checks pass, add key to key cache and Pub object to list..
+                    self.pub_keys.append(key)
+                    self.pubs[key] = Pub(value)
+
+            logging.debug(f"{filename} pub db entries are OK.")
 
 
 
@@ -128,120 +238,17 @@ class PublicationValidator(ConfigurationValidator):
 
 
 
-
-
-
-
-
-
-
-# ----------------------------------------------------------------------
-
-field_keys = ['block-height', 'block-width', 'color', 'cover-height', 
-              'cover-width', 'title']
-
-
-with open('configure.toml', 'rb') as config_file:
-    config_data = tomllib.load(config_file)
-
-
-with open('publication.toml', 'rb') as publication_file:
-    publication_data = tomllib.load(publication_file)
-
-
-def valid_color(color):
-    return color in config_data['validation']['colors']
-
-
-def valid_block_height(block_height):
-    a, b = config_data['validation']['block-limits']
-    return a <= block_height <= b
-
-
-def valid_block_width(block_width):
-    a, b = config_data['validation']['block-limits']
-    return a <= block_width <= b
-
-
-def valid_cover_height(cover_height):
-    a, b = config_data['validation']['cover-limits']
-    return a <= cover_height <= b
-
-
-def valid_cover_width(cover_width):
-    a, b = config_data['validation']['cover-limits']
-    return a <= cover_width <= b
-
-
-def validation_report(publication_key, issue_key, message):
-    print()
-    print(message + ' ...')
-    for field_key in field_keys:
-        if field_key == issue_key:
-            print('{0} = {1}   <===== *** HERE ***'
-                  .format(field_key, publication_data[publication_key][field_key]))
-        else:
-
-            print('{0} = {1}'
-                  .format(field_key, publication_data[publication_key][field_key]))
-    print()
-
-
-class PublicationTable:
-
-    def __init__(self, publication_filename):
-        self.headers = ['key', 'title', 'BH', 'BW', 'CH', 'CW', 'CLR']
-        self.filename = publication_filename
-        with open(self.filename, 'rb') as f:
-            self.data = tomllib.load(f)
-        self.table = [[k, v['title'], 
-                          v['block-height'], 
-                          v['block-width'],
-                          v['cover-height'], 
-                          v['cover-width'], 
-                          v['color']] 
-                      for k, v in self.data.items()] 
-        self.table_string = tabulate(sorted(self.table, 
-                                            key=operator.itemgetter(0), 
-                                            reverse=False), 
-                                     self.headers, 
-                                     tablefmt="grid", 
-                                     floatfmt=".4f")  
-             
 
 if __name__ == '__main__':
-#    warning = {'color':False, 'block-height':False,
-#               'block-width':False, 'cover-height':False,
-#               'cover-width':False}
-#    for publication_key, val in publication_data.items():    
-#
-#        if not valid_color(val['color']):
-#            validation_report(publication_key, 'color', 'Warning: unknown color')
-#            warning['color'] = True
-#        
-#        if not valid_block_height(val['block-height']):
-#            validation_report(publication_key, 'block-height', 'Warning: bad block height')
-#            warning['block-height'] = True
-#        
-#        if not valid_block_width(val['block-width']):
-#            validation_report(publication_key, 'block-width', 'Warning: bad block width')
-#            warning['block-width'] = True
-#        
-#        if not valid_cover_height(val['cover-height']):
-#            validation_report(publication_key, 'cover-height', 'Warning: bad cover height')
-#            warning['cover-height'] = True
-#
-#        if not valid_cover_width(val['cover-width']):
-#            validation_report(publication_key, 'cover-width', 'Warning: bad cover width')
-#            warning['cover-width'] = True
-#
-#    if not warning['color']: print('colors ok.')
-#    if not warning['block-height']: print('block heights ok.')
-#    if not warning['block-width']: print('block widths ok.')
-#    if not warning['cover-height']: print('cover heights ok.')
-#    if not warning['cover-width']: print('cover widths ok.')
+    try:
+        PV = PubValidator()
+    except ConfigError as e:
+        print(e)
+        exit(1)
+    except PubError as pe:
+        print(pe)
+        exit(1)
 
-    my = PublicationValidator()
 
 
 
